d1b91a5927f20891d63abe581928cd6109b80c1d
paddy <paddy@MacBookPro.fritz.box>
2025-10-11 11:40:59 +0200

    password-change

--- app-backend/api/password_hash.php
+++ app-backend/api/password_hash.php
@@ -1,4 +1,4 @@
 <?php
-$newPassword = '2025:IwM,iwFid5!'; // Replace with your desired password
+$newPassword = 'null'; // Replace with your desired password
 $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
 echo $hashedPassword;
